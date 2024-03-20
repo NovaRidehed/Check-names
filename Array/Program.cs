b7851ab6025eaf5b20fec1e3a5f6979d428a60a0
Nova Ridehed <nova.ridehed@skola.sala.se>
2024-03-20 12:35:46 +0100

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Demonstration av Contains
+            string[] names = { "Anna", "Bertil", "Cesar" };
+            Console.WriteLine("Demonstration av Contains:");
+            Console.WriteLine("Contains(Bertil): " + Contains(names, "Bertil")); // true
+            Console.WriteLine("Contains(David): " + Contains(names, "David")); // false
+        }
+        public static bool Contains(string[] names, string name)
+        {
+            foreach(string s in names)
+            {
+                bool itsAMatch = (s == name);
+                if (itsAMatch == true)
+                {
+                    return true;
+                }
+            }
+            
+            return false; // TODO
+        }
+    }
+}
